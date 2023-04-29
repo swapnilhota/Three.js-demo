a70770240fd69e40d4494ff15b99265d3784c22b
Swapnil Hota <swapnilhota99@gmail.com>
2023-04-30 00:51:49 +0530

    added controls to move around the bg

--- project/main.js
+++ project/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from "three";
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -31,13 +32,20 @@ const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function animate() {
   requestAnimationFrame(animate);
 
+  // defines rotation speed of object
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  // helps move around the background with mouse
+  controls.update();
+
+  // renders all the changes
   renderer.render(scene, camera);
 }
 

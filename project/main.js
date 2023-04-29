cdb92229c481b04886a5ab27b109e9e1c872e482
Swapnil Hota <swapnilhota99@gmail.com>
2023-04-30 01:08:25 +0530

    added space texture in the bgc

--- project/main.js
+++ project/main.js
@@ -47,6 +47,9 @@ function addStar() {
 
 Array(200).fill().forEach(addStar); 
 
+const spaceTexture = new THREE.TextureLoader().load("./images/space.jpg");
+scene.background = spaceTexture;
+
 function animate() {
   requestAnimationFrame(animate);
 
